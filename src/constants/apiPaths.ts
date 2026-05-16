ffd55cb93aaed1d19ee87a443b278f6f685ed7b7
AlanKowalzky <alan.kowalzky@gmail.com>
2026-05-17 01:56:39 +0200

    task5 update

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://642wyzq699.execute-api.eu-central-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-central-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-central-1.amazonaws.com/dev",
+  import: "https://eeaa54tcdc.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://642wyzq699.execute-api.eu-central-1.amazonaws.com/prod",
   cart: "https://.execute-api.eu-central-1.amazonaws.com/dev",
 };
